5d9637e9e09b45f40fa50ebbad25d21053bf8e33
AyoubTaoufik <ayoub107p5@gmail.com>
2022-06-05 19:57:21 +0100

    Added Initial folders & variables

--- mobile/config.ts
+++ mobile/config.ts
@@ -1,3 +1,3 @@
-export const ESP32_CAM_URI = "http://192.168.1.120";
+export const ESP32_CAM_URI = "ENTER YOUR ESP32-CAM IP ADDRESS"; // You will get it from the ESP32-CAM Serial on loading
 
-export const SERVER_URI = "http://192.168.1.102:5000";
+export const SERVER_URI = "ENTER THE FLASK SERVER ADDRESS HERE"; // You will get it from the FLASK server debug messages on Loading too
